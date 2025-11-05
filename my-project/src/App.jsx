1b96de68cd3367b16e664e52d729156f725d5d2c
KayasSecret <kayasmishra29s@gmail.com>
2025-11-05 17:23:49 +0530

    Basic_Setup_Reboot

--- my-project/src/App.jsx
+++ my-project/src/App.jsx
@@ -1,9 +1,13 @@
 import React from 'react'
 
-const App = () => {
+function App() {
   return (
-    <div>
-      
+    <div className="min-h-screen flex items-center justify-center bg-gray-900">
+      <h1 className="text-4xl font-bold text-blue-600">
+        Hello Tailwind 💙
+      </h1>
     </div>
   )
 }
+
+export default App
