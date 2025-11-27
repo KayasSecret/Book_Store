09d83020071e7bc85b381d5d0790edf99653f254
KayasSecret <kayasmishra29s@gmail.com>
2025-11-27 15:52:28 +0530

    #new_update

--- Frontend/src/components/Card.jsx
+++ Frontend/src/components/Card.jsx
@@ -1,11 +1,10 @@
 import React from 'react'
 
 function Card({ item }) {
-    console.log(item)
   return (
     <>
-        <div>
-           <div className="card bg-base-100 w-full max-w-sm shadow-sm mx-auto">
+        <div className='mt-4 my-3'>
+           <div className="card bg-base-100 w-90 max-w-sm shadow-xl mx-auto">
                 <figure>
                     <img
                     src={item.image}
@@ -17,9 +16,9 @@ function Card({ item }) {
                     <div className="badge badge-secondary">{item.category}</div>
                     </h2>
                     <p>{item?.title}</p>
-                    <div className="card-actions justify-end">
-                    <div className="badge badge-outline">Fashion</div>
-                    <div className="badge badge-outline">Products</div>
+                    <div className="card-actions justify-between">
+                    <div className="badge badge-outline">${item.price}</div>
+                    <div className="cursor-pointer px-2 py-1 rounded-full border-[2px] hover:bg-pink-500 hover:text-white duration-200">Buy now</div>
                     </div>
                 </div>
             </div>
--- Frontend/src/components/FreeBook.jsx
+++ Frontend/src/components/FreeBook.jsx
@@ -23,25 +23,25 @@ function FreeBook() {
             {
                 breakpoint: 1024,
                 settings: {
-                slidesToShow: 3,
-                slidesToScroll: 3,
-                infinite: true,
-                dots: true
+                    slidesToShow: 3,
+                    slidesToScroll: 3,
+                    infinite: true,
+                    dots: true
                 }
             },
             {
                 breakpoint: 600,
                 settings: {
-                slidesToShow: 2,
-                slidesToScroll: 2,
-                initialSlide: 2
+                    slidesToShow: 2,
+                    slidesToScroll: 2,
+                    initialSlide: 2
                 }
             },
             {
                 breakpoint: 480,
                 settings: {
-                slidesToShow: 1,
-                slidesToScroll: 1
+                    slidesToShow: 1,
+                    slidesToScroll: 1
                 }
             }
         ]
